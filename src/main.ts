c62058fdfad444e99eb89f565f52d9e774efadd4
HypoLast <mike.ball1289@gmail.com>
2020-04-06 14:08:35 -0400

    Read default todo items

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,28 @@ import { createServer } from 'http';
 
 const app = express();
 
-function wait(time: number) {
-    return new Promise(resolve => setTimeout(resolve, time));
+enum Status {
+    OPEN = 'open',
+    PROGRESS = 'progress',
+    COMPLETE = 'complete',
 }
 
-app.get('/', async (req, res) => {
-    res.end(`hot reload is fun!`);
+interface TodoItem {
+    status: Status;
+    title: string;
+}
+
+let todos: TodoItem[] = [{
+    status: Status.PROGRESS,
+    title: 'Make a todo list',
+}, {
+    status: Status.OPEN,
+    title: 'Learn some nodejs',
+}];
+
+app.get('/todos', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(todos));
 });
 
 createServer(app).listen(8080, () => console.log('server started on http://localhost:8080'));
