31000e179ff52077ec25ff67b6411f057a19026a
HypoLast <mike.ball1289@gmail.com>
2020-04-06 13:48:38 -0400

    Basic promis stuff

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log('hello world');
+import express from 'express';
+import { createServer } from 'http';
+
+const app = express();
+
+function wait(time: number) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+let count = 0;
+app.get('/', async (req, res) => {
+    await wait(2000);
+    res.end(`hello world ${++ count}`);
+});
+
+createServer(app).listen(8080, () => console.log('server started on http://localhost:8080'));
